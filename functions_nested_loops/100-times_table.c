1d96b4c1e694ea56f4582b49a61c56568740813f
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-07-06 13:07:37 +0000

    Add advanced 100-times_table function

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the n times table.
+ * @n: The multiplication table to print.
+ */
+void print_times_table(int n)
+{
+	int i, j, res;
+
+	if (n < 0 || n > 15)
+		return;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			res = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (res < 10)
+					_putchar(' ');
+				if (res < 100)
+					_putchar(' ');
+				if (res >= 100)
+					_putchar((res / 100) + '0');
+				if (res >= 10)
+					_putchar(((res / 10) % 10) + '0');
+			}
+			_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
