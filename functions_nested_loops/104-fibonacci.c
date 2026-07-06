bd6da15d7c25fa467ea6e261e86ddb18626b5df9
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-07-06 13:24:51 +0000

    Fix Fibonacci 98 output formatting

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -17,7 +17,12 @@ int main(void)
 		carry = (a2 + b2) / 1000000000000000000;
 		next2 = (a2 + b2) % 1000000000000000000;
 		next1 = a1 + b1 + carry;
-		printf(", %lu%018lu", next1, next2);
+		
+		if (next1 > 0)
+			printf(", %lu%018lu", next1, next2);
+		else
+			printf(", %lu", next2);
+			
 		a1 = b1;
 		a2 = b2;
 		b1 = next1;
