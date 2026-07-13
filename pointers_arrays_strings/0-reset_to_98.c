7470fa81b239b5a8be53dfebca094d98de0be193
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-07-13 11:20:53 +0000

    Fix Betty style for 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - takes a pointer to an int and updates the value
+ * @n: input pointer
+ * Return: nothing
+ */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
