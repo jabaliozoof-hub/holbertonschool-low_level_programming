cfe3c102492aea80ad21727713e5bb0414cf3aae
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-07-15 09:52:15 +0000

    Add 7-leet solution

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ * @s: pointer to the string.
+ *
+ * Return: pointer to the encoded string.
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char l[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			
+			if (s[i] == l[j])
+			{
+				s[i] = n[j];
+			}
+		}
+	}
+	return (s);
+}
