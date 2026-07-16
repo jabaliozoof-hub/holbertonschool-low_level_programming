5cb4691039fe4d6035b28277aad0dc3a3e86aa07
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-07-16 14:41:03 +0000

    Implement set_string (double pointer) and add its prototype

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to the pointer to be set
+ * @to: pointer to the string to set it to
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
