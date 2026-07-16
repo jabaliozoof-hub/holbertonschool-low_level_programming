e76af4c45e3f67eea6da242bda9a32ea65b222c4
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-07-16 10:47:44 +0000

    Add 103-infinite_add.c solution

---
+++ pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+int get_len(char *n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+    int i, j, k, l1, l2, sum, carry = 0;
+
+    l1 = get_len(n1);
+    l2 = get_len(n2);
+    if (l1 + 2 > size_r || l2 + 2 > size_r)
+        return (0);
+    
+    i = l1 - 1;
+    j = l2 - 1;
+    k = 0;
+    while (k < size_r - 1)
+    {
+        sum = carry;
+        if (i >= 0)
+            sum += n1[i--] - '0';
+        if (j >= 0)
+            sum += n2[j--] - '0';
+        if (i < 0 && j < 0 && sum == 0)
+            break;
+        r[k++] = (sum % 10) + '0';
+        carry = sum / 10;
+    }
+    if (i >= 0 || j >= 0 || carry)
+        return (0);
+    r[k] = '\0';
+    for (i = 0; i < k / 2; i++)
+    {
+        char tmp = r[i];
+        r[i] = r[k - 1 - i];
+        r[k - 1 - i] = tmp;
+    }
+    return (r);
+}
+
+int get_len(char *n)
+{
+    int len = 0;
+    while (n[len])
+        len++;
+    return (len);
+}
