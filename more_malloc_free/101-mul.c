563d75ed0fa5c8f1eb04a61d23cbf8973cd01144
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-07-21 10:47:56 +0000

    101-mul: refactor into smaller functions for Betty style compliance

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -51,6 +51,67 @@ int is_digit(char *s)
 }
 
 /**
+ * check_args - validates argument count and digit content
+ * @argc: argument count
+ * @argv: argument vector
+ */
+void check_args(int argc, char *argv[])
+{
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+		print_error();
+}
+
+/**
+ * multiply - multiplies two number strings and stores result in array
+ * @s1: first number string
+ * @s2: second number string
+ * @result: array to store the result digits
+ * @len_result: length of the result array
+ */
+void multiply(char *s1, char *s2, int *result, int len_result)
+{
+	int i, j, n1, n2, carry, len1, len2;
+
+	(void)len_result;
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		n1 = s1[i] - '0';
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = s2[j] - '0';
+			carry += result[i + j + 1] + (n1 * n2);
+			result[i + j + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry)
+			result[i + j + 1] += carry;
+	}
+}
+
+/**
+ * print_result - prints the result array, skipping leading zeros
+ * @result: array containing the result digits
+ * @len_result: length of the result array
+ */
+void print_result(int *result, int len_result)
+{
+	int i, lead = 0;
+
+	for (i = 0; i < len_result; i++)
+	{
+		if (result[i])
+			lead = 1;
+		if (lead || i == len_result - 1)
+			_putchar(result[i] + '0');
+	}
+	_putchar('\n');
+}
+
+/**
  * main - multiplies two positive numbers
  * @argc: argument count
  * @argv: argument vector
@@ -60,13 +121,14 @@ int is_digit(char *s)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len_result, i, j, n1, n2, carry, *result, lead = 0;
+	int len1, len2, len_result, i, *result;
 
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-		print_error();
+	check_args(argc, argv);
 
-	s1 = argv[1], s2 = argv[2];
-	len1 = _strlen(s1), len2 = _strlen(s2);
+	s1 = argv[1];
+	s2 = argv[2];
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	len_result = len1 + len2;
 
 	result = malloc(sizeof(int) * len_result);
@@ -76,30 +138,9 @@ int main(int argc, char *argv[])
 	for (i = 0; i < len_result; i++)
 		result[i] = 0;
 
-	for (len1 = len1 - 1; len1 >= 0; len1--)
-	{
-		n1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1, j = len2; j >= 0; j--)
-		{
-			n2 = s2[j] - '0';
-			carry += result[len1 + j + 1] + (n1 * n2);
-			result[len1 + j + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry)
-			result[len1 + j + 1] += carry;
-	}
+	multiply(s1, s2, result, len_result);
+	print_result(result, len_result);
 
-	for (i = 0; i < len_result; i++)
-	{
-		if (result[i])
-			lead = 1;
-		if (lead || i == len_result - 1)
-			_putchar(result[i] + '0');
-	}
-
-	_putchar('\n');
 	free(result);
 	return (0);
 }
