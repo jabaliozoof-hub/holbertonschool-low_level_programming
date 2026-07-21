49a91d0c0813a6ec97ee0d160f1ffeee0135b132
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-07-21 10:37:34 +0000

    101-mul: fix multiple definition of _putchar

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -3,18 +3,6 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * _strlen - returns the length of a string
  * @s: string to evaluate
  *
@@ -76,14 +64,18 @@ int main(int argc, char *argv[])
 
 	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 		print_error();
+
 	s1 = argv[1], s2 = argv[2];
 	len1 = _strlen(s1), len2 = _strlen(s2);
 	len_result = len1 + len2;
+
 	result = malloc(sizeof(int) * len_result);
 	if (!result)
 		return (1);
+
 	for (i = 0; i < len_result; i++)
 		result[i] = 0;
+
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		n1 = s1[len1] - '0';
@@ -98,6 +90,7 @@ int main(int argc, char *argv[])
 		if (carry)
 			result[len1 + j + 1] += carry;
 	}
+
 	for (i = 0; i < len_result; i++)
 	{
 		if (result[i])
@@ -105,6 +98,7 @@ int main(int argc, char *argv[])
 		if (lead || i == len_result - 1)
 			_putchar(result[i] + '0');
 	}
+
 	_putchar('\n');
 	free(result);
 	return (0);
