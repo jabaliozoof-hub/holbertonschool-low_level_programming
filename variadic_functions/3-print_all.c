da09a3a9e82b33467a83202a3422d35195266ac5
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-08-01 13:12:39 +0000

    Fix constraints in task 3 using switch case

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,80 +3,40 @@
 #include <stdarg.h>
 
 /**
- * print_char - prints a char
- * @args: argument list
- */
-void print_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-
-/**
- * print_int - prints an int
- * @args: argument list
- */
-void print_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-
-/**
- * print_float - prints a float
- * @args: argument list
- */
-void print_float(va_list args)
-{
-	printf("%f", (float)va_arg(args, double));
-}
-
-/**
- * print_string - prints a string
- * @args: argument list
- */
-void print_string(va_list args)
-{
-	char *str = va_arg(args, char *);
-
-	if (!str)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-
-/**
  * print_all - Prints anything based on format.
  * @format: list of types passed to the function.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0, j;
-	char *sep = "";
-	const char types[] = "cifs";
+	int i = 0;
+	char *sep = "", *str;
 
 	va_start(args, format);
 	while (format && format[i])
 	{
-		j = 0;
-		while (types[j])
+		switch (format[i])
 		{
-			if (format[i] == types[j])
-			{
-				printf("%s", sep);
-				if (j == 0)
-					print_char(args);
-				if (j == 1)
-					print_int(args);
-				if (j == 2)
-					print_float(args);
-				if (j == 3)
-					print_string(args);
-				sep = ", ";
-			}
-			j++;
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, (float)va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+			default:
+				i++;
+				continue;
 		}
+		sep = ", ";
 		i++;
 	}
 	printf("\n");
