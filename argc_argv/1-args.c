f3b264d0b7e17c2506db90978bed708a0bf47bdd
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-08-01 19:57:08 +0000

    C - argc, argv, task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: the number of arguments
+ * @argv: an array of pointers to the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
