5f08f2f3b436c5fd7d20e403dc811dbed80b7cf6
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-08-09 14:04:14 +0300

    Fix Betty style warnings in 0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX standard output.
+ * read_textfile - Reads a text file and prints
+ *                 it to POSIX standard output.
  * @filename: The name of the file to read.
- * @letters: The number of letters it should read and print.
+ * @letters: Number of letters to read and print.
  *
- * Return: The actual number of letters it could read and print, or 0 on failure.
+ * Return: Actual number of letters read and printed, or 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -13,24 +14,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nrd, nwr;
 	char *buf;
 
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (0);
-	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
 		return (0);
-	}
-
 	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	if (!buf)
 	{
 		close(fd);
 		return (0);
 	}
-
 	nrd = read(fd, buf, letters);
 	if (nrd == -1)
 	{
@@ -38,17 +32,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	nwr = write(STDOUT_FILENO, buf, nrd);
-	if (nwr == -1 || nwr != nrd)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-
 	free(buf);
 	close(fd);
+	if (nwr == -1 || nwr != nrd)
+		return (0);
 	return (nwr);
 }
-
