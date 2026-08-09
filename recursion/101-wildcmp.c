b2212d116243e94f1936bd4283f8301edcb81f28
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-08-09 11:29:29 +0300

    Complete final advanced recursion task 101-wildcmp.c

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares two strings, considering the special character *.
+ * @s1: The first string to compare.
+ * @s2: The second string (can contain *).
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
+
