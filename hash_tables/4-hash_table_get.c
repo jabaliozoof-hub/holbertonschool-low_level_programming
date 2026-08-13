b90b3de72af9f8948c5f8c814f605410cfaa233e
jabaliozoof-hub <jabaliozoof@gmail.com>
2026-08-13 19:37:00 +0000

    Implement hash_table_get function

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: pointer to the hash table
+ * @key: key you are looking for
+ *
+ * Return: value associated with the element, or NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
